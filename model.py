bbc64daa39f8a8c8c0c5da85810f5fb8735ce00e
Sebastian Villegas <svillegasz@192.168.1.7>
2024-06-20 20:12:10 -0500

    initial commit

---
+++ model.py
@@ -0,0 +1,16 @@
+# Read data set
+
+
+# Pre process data (NLP)
+
+
+# Data augmentation https://neptune.ai/blog/data-augmentation-nlp
+
+
+# Feature augmentation
+
+
+# Herachichal Attention Network
+
+
+# Evaluation
